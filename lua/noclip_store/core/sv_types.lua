478dc7360fb850e58756a3e9770b30fedd72d3b1
Owain <owain.jones74@gmail.com>
2021-12-05 04:30:57 +0000

    pointshop 1 and 2 support for online players
    Need to figure out how to handle offline players. Might have to have events not process till the player is online?

--- lua/noclip_store/core/sv_types.lua
+++ lua/noclip_store/core/sv_types.lua
@@ -1,3 +1,5 @@
+-- If this file becomes a bit of a mess, we can make a subfolder and give them each their own file in there. Future issue tho.
+
 -- Future proofing with expired arg
 -- Also need to account for a lot more admin systems
 NoClip.Store.Types['rank'] = function(plyID, expired, data)
@@ -28,5 +30,25 @@ NoClip.Store.Types['rank'] = function(plyID, expired, data)
 	-- No admin system found
 	else
 		NoClip.Store.Core.Error("Attempted to assign "..plyID.." the rank "..rank..", but no admin system was found.")
+	end
+end
+-- Pointshop 1 points
+NoClip.Store.Types['pointshop_1_points'] = function(plyID, expired, data)
+	local ply = player.GetBySteamID64(plyID)
+
+	if ply then -- Online
+		ply:PS_GivePoints(data.data.points)
+	else -- Offline
+		-- Currently not sure the 'correct' way to give offline players points rn
+	end
+end
+-- Pointshop w points
+NoClip.Store.Types['pointshop_2_points'] = function(plyID, expired, data)
+	local ply = player.GetBySteamID64(plyID)
+	
+	if ply then -- Online
+		ply:PS2_AddStandardPoints(data.data.points)
+	else -- Offline
+		-- Also not sure how to handle this rn
 	end
 end
