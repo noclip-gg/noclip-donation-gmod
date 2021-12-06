0530cbcac73cef9e8d667e1e8b75f904450d147a
Owain <owain.jones74@gmail.com>
2021-12-06 09:45:11 +0000

    premium pointshop 2 points

--- lua/noclip_store/core/sv_types.lua
+++ lua/noclip_store/core/sv_types.lua
@@ -57,3 +57,8 @@ NoClip.Store.Types['pointshop_2_points'] = function(ply, expired, data)
 	if not ply.PS2_AddStandardPoints then return end
 	ply:PS2_AddStandardPoints(data.data.points)
 end
+-- Pointshop 2 Premium Points
+NoClip.Store.Types['pointshop_2_premium_points'] = function(ply, expired, data)
+	if not ply.PS2_AddPremiumPoints then return end
+	ply:PS2_AddPremiumPoints(data.data.points)
+end
