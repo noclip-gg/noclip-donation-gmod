34538ec4ba2c745b686757adaada0da155d7038d
Owain <owain.jones74@gmail.com>
2021-12-06 09:53:16 +0000

    levels action
    Need to add support for more systems

--- lua/noclip_store/core/sv_types.lua
+++ lua/noclip_store/core/sv_types.lua
@@ -67,3 +67,14 @@ NoClip.Store.Types['darkrp_money'] = function(ply, expired, data)
 	if not DarkRP then return end
 	ply:addMoney(data.data.money)
 end
+-- DarkRP Levels
+NoClip.Store.Types['darkrp_levels'] = function(ply, expired, data)
+	-- Vrondakis' leveling system (https://github.com/uen/Leveling-System)
+	if LevelSystemConfiguration then
+		ply:addLevels(data.data.levels)
+
+	-- No level system found
+	else
+		NoClip.Store.Core.Error("Attempted to assign "..ply:SteamID().." "..data.data.levels.." DarkRP levels, but no level system was found.")
+	end
+end
