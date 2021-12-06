24bf384ee5131562ff9cbf0fd0769646b4a01582
Owain <owain.jones74@gmail.com>
2021-12-06 09:25:41 +0000

    escape players name

--- lua/noclip_store/core/sv_types.lua
+++ lua/noclip_store/core/sv_types.lua
@@ -25,7 +25,7 @@ end
 -- Console Command
 NoClip.Store.Types['custom_console_command'] = function(ply, expired, data)
 	local command = data.data.console_command
-	command = string.Replace(command, "{player.name}", "\""..ply:Name().."\"")
+	command = string.Replace(command, "{player.name}", "\""..string.Replace(ply:Name(), "\"", "").."\"")
 	command = string.Replace(command, "{player.steamID}", ply:SteamID())
 	command = string.Replace(command, "{player.steamID64}", ply:SteamID64())
 
