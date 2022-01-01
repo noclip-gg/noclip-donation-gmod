d266d4c13a1cf8de94eb16ae4168f5a0546b2557
Owain <owain.jones74@gmail.com>
2022-01-01 23:18:02 +0000

    1.1 changes (#3)
    * pass event data to actions
    
    * implement player.name to Custom Lua
    
    * implement purchase.amount to Custom Lua and Custom Console Commands
    
    Resolves #2

--- lua/noclip_store/core/types/command.lua
+++ lua/noclip_store/core/types/command.lua
@@ -1,11 +1,12 @@
 -- Custom Console Command
-NoClip.Store.Core.RegisterType("custom_console_command", function(ply, expired, data)
+NoClip.Store.Core.RegisterType("custom_console_command", function(ply, expired, data, eventData)
 	local command = expired and (data.data.console_command_expire or "") or data.data.console_command
 	if (not command) or (command == "") then return end
 
 	command = string.Replace(command, "{player.name}", "\""..string.Replace(ply:Name(), "\"", "").."\"")
 	command = string.Replace(command, "{player.steamID}", ply:SteamID())
 	command = string.Replace(command, "{player.steamID64}", ply:SteamID64())
+	command = string.Replace(command, "{purchase.amount}", eventData.purchase_amount)
 
 	game.ConsoleCommand(command.."\n")
 end)
