8f6da56df7348697706d882ce83ba905a1bce632
blu <bluiscool1337@gmail.com>
2022-09-12 15:58:10 -0500

    refactor: put the new routes in the config

--- lua/noclip_store/config/sv_config.lua
+++ lua/noclip_store/config/sv_config.lua
@@ -1,10 +1,11 @@
--- Your community's API key. Can be found here: https://mycommunity.noclip.me/dashboard/settings
+-- Your community's API key. Can be found here: https://yourcommunity.noclip.me/admin/general-settings
+-- if you don't see the API key, press "Reset API Key" at the top
 NoClip.Store.Config.APIKey = "ABC-123-XYZ"
 
 -- The URL to your community. Ensure there is no trailing "/"
-NoClip.Store.Config.URL = "https://mycommunity.noclip.me"
+NoClip.Store.Config.URL = "https://yourcommunity.noclip.me"
 
--- This server's ID. It can be found by selecting the server here: https://mycommunity.noclip.me/dashboard/servers
+-- This server's ID. It can be found by selecting the server here: https://yourcommunity.noclip.me/admin/servers
 NoClip.Store.Config.ServerID = "ABC123"
 
 -- How often (in seconds) should the server check for any new purchases?
